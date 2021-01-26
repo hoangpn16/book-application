27ce0d232aa9075133484bd8566fca5d3b441290
Phan Hoang <phanhoang@Phans-MacBook-Pro.local>
2021-01-26 11:27:19 +0700

    update code

--- src/main/java/projectbookapplication/controller/UserController.java
+++ src/main/java/projectbookapplication/controller/UserController.java
@@ -11,30 +11,40 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping(value ="/user")
+@RequestMapping(value = "/user")
 public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping(value = "/getall",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public List<User> getAllUsers(){
+    @GetMapping(value = "/getall", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<User> getAllUsers() {
         List<User> listUser = userService.getAllUser();
         return listUser;
     }
-    @GetMapping(value = "/{phoneNumber}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public User getUserByPhoneNumber(@PathVariable(name="phoneNumber")String phoneNumber){
-        User result=userService.getUserByPhoneNumbers(phoneNumber);
+
+    @GetMapping(value = "/{phoneNumber}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public User getUserByPhoneNumber(@PathVariable(name = "phoneNumber") String phoneNumber) {
+        User result = userService.getUserByPhoneNumbers(phoneNumber);
         return result;
     }
-    @PostMapping(value="/insert",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public @ResponseBody String registerUser(@RequestBody User user){
+
+    @PostMapping(value = "/insert", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public @ResponseBody
+    String registerUser(@RequestBody User user) {
         userService.insertUser(user);
         return "Register Successfully";
     }
-    @PutMapping(value ="/changepassword",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public String changeUserPassword(@PathVariable(value = "phonenumber") String phonenumber, @RequestParam(value = "password") String password){
+
+    @PutMapping(value = "/changepassword", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public String changeUserPassword(@PathVariable(value = "phonenumber") String phonenumber, @RequestParam(value = "password") String password) {
         userService.changePassword(phonenumber, password);
         return "Change user's password successfully";
+    }
+
+    @GetMapping(value = "/login")
+    public User loginUser(@RequestParam(value = "phonenumber") String phonenumber, @RequestParam(value = "password") String password) {
+        User user = userService.loginUser(phonenumber, password);
+        return user;
     }
 
 }
